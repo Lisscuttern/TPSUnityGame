9fd4b8208ea41f4b49fd9420d18b5cc2cb33f1f5
Ozan <73613081+Lisscuttern@users.noreply.github.com>
2022-06-04 17:23:48 -0700

    ++ ADD : Bullet collect

--- My project/Assets/Scripts/Player.cs
+++ My project/Assets/Scripts/Player.cs
@@ -10,25 +10,62 @@ public class Player : MonoBehaviour
     #region SerializeFields
 
     [SerializeField] private Rigidbody m_rb;
-    [SerializeField] private DynamicJoystick m_dynamicJoyStick;
+    [SerializeField] private FloatingJoystick m_floatingJoystick;
     [SerializeField] private float m_speed;
+    [SerializeField] private GameObject[] bullet;
 
     #endregion
+
+    private GameObject targetBullet;
 
     private void FixedUpdate()
     {
         Movement();
     }
 
+    /// <summary>
+    /// This function help for player move on the ground
+    /// </summary>
     private void Movement()
     {
-        if (transform.position.x < -9.5f || transform.position.x > 9.5f)
-            return;
-        
         m_rb.velocity = new Vector3(
-            m_dynamicJoyStick.Horizontal * m_speed,
+            m_floatingJoystick.Horizontal * m_speed,
             m_rb.velocity.y,
-            m_dynamicJoyStick.Vertical * m_speed
+            m_floatingJoystick.Vertical * m_speed
         );
+    }
+
+    /// <summary>
+    /// this function help for collect bullet from ground
+    /// </summary>
+    private void BulletCollect()
+    {
+        bullet[0] = targetBullet;
+        targetBullet.transform.parent = transform;
+        targetBullet.transform.DOLocalJump(new Vector3(0, 1, 0),1f,1,0.5f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Bullet")
+        {
+            if (bullet[0] != null)
+                return;
+            targetBullet = other.gameObject.GetComponent<BulletComponent>().gameObject;
+            BulletCollect();
+
+            // bullet[0] = other.gameObject;
+            //
+            // other.transform.parent = transform;
+            // other.gameObject.transform.DOLocalJump(new Vector3(0, 1, 0),1f,1,0.5f);
+        }
+    }
+
+    /// <summary>
+    /// This function help for fire the bullet 
+    /// </summary>
+    private void Fire()
+    {
+        
     }
 }
